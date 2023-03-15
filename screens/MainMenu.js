f05c141ad451f80bb2dbb91a8b53a93e6524a222
atsadawutcs <atsadawut.h@kkumail.com>
2023-03-15 14:31:12 +0700

    Edit: screens/MainMenu.js

--- screens/MainMenu.js
+++ screens/MainMenu.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Dimensions, Button,Image } from 'react-native'
+import { StyleSheet, Text, View, Dimensions, Button, Image } from 'react-native'
 import React from 'react'
 import Circlelar from './Circlelar'
 
@@ -11,32 +11,34 @@ const TotalAnswer = () => {
     return (
         <View >
             <View style={styles.section1}>
-                
+
             </View>
             <View style={styles.section2}>
-            <Image source={require('../assets/quizicon.png')}
+                <Image source={require('../assets/quizicon.png')}
                     style={{ width: 200, height: 200 }}
                 />
-               
+
             </View>
-            <View style={styles.section}>
+            <View style={styles.title}>
                 <Button color="#FFDD40" style={styles.btn} title="เลือกหมวดหมู่คำถาม"></Button>
             </View>
-            <View >
+            <View style={styles.title}>
                 <Button color="#FF4155" style={styles.btn} title="กระดานคะแนน"></Button>
             </View>
-            <View>
+            <View style={styles.title}>
                 <Button color="#38C674" style={styles.btn} title="ผู้พัฒนา"></Button>
             </View>
-            <View>
+            <View style={styles.title}>
                 <Button color="#0053CE" style={styles.btn} title="ออกจากระบบ"></Button>
             </View>
-            <View>
-            <Image source={require('../assets/profileicon.png')}
+            <View style={styles.title2}>
+                <Image source={require('../assets/profileicon.png')}
                     style={{ width: 80, height: 80 }}
                 />
+                <Text style={styles.txt2}>บัญชีผู้ใช้</Text>
+
             </View>
-           
+
         </View>
     );
 }
@@ -45,12 +47,12 @@ export default TotalAnswer
 
 const styles = StyleSheet.create({
     Button: {
-        flexDirection: 'row', 
+        flexDirection: 'row',
         height: 50,
         alignItems: 'center',
         justifyContent: 'center',
         marginTop: 50,
-        elevation:3,
+        elevation: 3,
     },
 
     section1: {
@@ -59,35 +61,46 @@ const styles = StyleSheet.create({
     },
     section2: {
         width: width - 40,
-        height: height/4,
-        alignItems:'center',
+        height: height / 4,
+        alignItems: 'center',
     },
     section3: {
-        flexDirection:'row',
-        justifyContent:'space-around',
-        alignItems:'center',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
         width: width - 40,
-        height: height/4,
-        alignItems:'center',
+        height: height / 4,
+        alignItems: 'center',
     },
-    txt1:{
-        fontSize:40,
-        fontWeight:'800',
-        margin:10,
-        color:'#565656'
+    txt1: {
+        fontSize: 40,
+        fontWeight: '800',
+        margin: 10,
+        color: '#565656'
     },
-    txt2:{
-        fontSize:20,
-        fontWeight:'500',
-        color:'#565656'
+    txt2: {
+        fontSize: 15,
+        fontWeight: '500',
+        color: '#474747'
     },
-    txt3:{
-        fontSize:32,
-        fontWeight:'700',
-        margin:20,
-        color:'#565656'
+    txt3: {
+        fontSize: 32,
+        fontWeight: '700',
+        margin: 20,
+        color: '#565656'
     },
-    btn:{
-        borderRadius:10,
-    }
+    btn: {
+        borderRadius: 10,
+    },
+    title: {
+        textAlign: 'center',
+        marginVertical: 8,
+    },
+    title2: {
+        textAlign: 'center',
+        marginVertical: 10,
+        width: width - 40,
+        height: height / 4,
+        alignItems: 'center',
+    },
 });
