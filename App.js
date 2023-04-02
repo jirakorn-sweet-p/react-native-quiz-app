29423e3360c7a736a86c7fb30912ec1d5b1d2c35
AliceSzPOL <IsuIFirstI1112@gmail.com>
2023-04-02 20:19:32 +0700

    .

--- App.js
+++ App.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-=======
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions} from 'react-native';
-import TotalAnswer from './screens/TotalAnswer';
-import Welcome from './screens/Welcome';
-import LoginSelect from './screens/LoginSelect';
-import Pin from './screens/Pin';
-import MainMenu from './screens/MainMenu';
-import ScoreBoard from './screens/ScoreBoard';
-import Profile from './screens/Profile';
-import Developer from './screens/Developer';
->>>>>>> e2d862d110f4c17e09e169d158ab847525f7b2d2
 
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
@@ -23,20 +10,10 @@ const { width, height } = Dimensions.get('window');
 export default function App() {
   
   return (
-<<<<<<< HEAD
     <NavigationContainer style={styles.container}>
       <RunApp></RunApp>
     </NavigationContainer>
     
-=======
-
-        //<ScoreBoard></ScoreBoard>
-        //<TotalAnswer></TotalAnswer>
-        <Developer></Developer>
-
-    
-
->>>>>>> e2d862d110f4c17e09e169d158ab847525f7b2d2
   );
 }
 
