e2d862d110f4c17e09e169d158ab847525f7b2d2
atsadawutcs <atsadawut.h@kkumail.com>
2023-04-02 16:48:46 +0700

    Edit Developer.js

--- screens/Developer.js
+++ screens/Developer.js
@@ -18,26 +18,25 @@ const Developer = () => {
             <View style={styles.container1}>
                 <View style={styles.container2}>
                     {/* Developer Name */}
-                    <View style={styles.container3}>
-                        <View style={styles.container_name}>
-                            <Text style={styles.txt_name}>นายอัษฎาวุธ หอมดี</Text>
+
+                    <View style={styles.container4}>
+                    <Image source={require('../assets/dev3.jpg')}
+                                style={{ width: 320, height: 200 }}
+                            />
+
+                        <View style={styles.container5}>
+                            <Text style={styles.txt_nameD1}>Mr.Atsadawut Homdee</Text>
+                            <Text style={styles.txt_nameD2}>Mr.Apichai Tuppoota</Text>
+                            <Text style={styles.txt_nameD3}>Mr.Jirakorn Donhuabo</Text>
+                            <Text style={styles.txt_nameD4}>Ms.Supaporn Krungsaenmueang</Text>
+
+                            <Text style={styles.txt_kku}>College of Computing, KKU</Text>
+                            
                         </View>
+                        
+
                     </View>
-                    <View style={styles.container3}>
-                        <View style={styles.container_name}>
-                            <Text style={styles.txt_name}>นางสาวสุภาภรณ์ กรุงแสนเมือง</Text>
-                        </View>
-                    </View>
-                    <View style={styles.container3}>
-                        <View style={styles.container_name}>
-                            <Text style={styles.txt_name}>นายอภิชัย ทัพภูตา</Text>
-                        </View>
-                    </View>
-                    <View style={styles.container3}>
-                        <View style={styles.container_name}>
-                            <Text style={styles.txt_name}>นายจิรากร ดอนหัวบ่อ</Text>
-                        </View>
-                    </View>
+
                 </View>
             </View>
         </View>
@@ -81,6 +80,23 @@ const styles = StyleSheet.create({
         paddingLeft: 50,
         //backgroundColor: 'yellow',
     },
+    container_name1: {
+        //marginTop: -5,
+        width: '50%',
+        height: '100%',
+        justifyContent: 'center',
+        //alignItems: 'center',
+        paddingLeft: 65,
+        //backgroundColor: 'yellow',
+    },
+    container_name2: {
+        width: '50%',
+        height: '100%',
+        justifyContent: 'center',
+        //alignItems: 'center',
+        //paddingLeft: 50,
+        //backgroundColor: 'purple',
+    },
     container1: {
         flex: 1,
         //alignItems: 'center',
@@ -105,26 +121,48 @@ const styles = StyleSheet.create({
     },
     container2: {
         flex: 1,
-        height: '100%',
+        height: '90%',
         width: '100%',
-        flexDirection: 'column',
-        marginTop: 7,
+        flexDirection: 'row',
+        marginTop: -20,
         marginBottom: 20,
         borderRadius: 10,
         padding: 20,
-        alignItems: 'center',
+        //alignItems: 'center',
         //backgroundColor: 'green',
     },
     container3: {
-        marginTop: 10,
+        marginTop: -5,
         height: '10%',
         width: '100%',
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
+        //backgroundColor: 'green',
         //backgroundColor: 'purple',
     },
-     title: {
+    container4: {
+        marginTop: 20,
+        height: '100%',
+        width: '100%',
+        flexDirection: 'column',
+        //justifyContent: 'center',
+        alignItems: 'center',
+        //backgroundColor: 'green',
+        //backgroundColor: 'purple',
+    },
+    container5: {
+        marginTop: 5,
+        height: '100%',
+        width: '100%',
+        flexDirection: 'column',
+        //justifyContent: 'center',
+        alignItems: 'center',
+        //backgroundColor: 'green',
+        //backgroundColor: 'purple',
+    }
+    ,
+    title: {
         width: '110%',
         height: '100%',
         justifyContent: 'center',
@@ -134,13 +172,13 @@ const styles = StyleSheet.create({
     txt_title: {
         paddingTop: 10,
         paddingBottom: 10,
-        paddingLeft: 10,
+        //paddingLeft: 10,
         textAlign: 'center',
         width: '100%',
         borderRadius: 10,
-        backgroundColor: '#FBCE30',
+        backgroundColor: '#4F4F4F',
         alignSelf: 'flex-end',
-        fontSize: 36,
+        fontSize: 30,
         fontWeight: '900',
         color: 'white',
         textShadowColor: "#000",
@@ -151,21 +189,116 @@ const styles = StyleSheet.create({
         textShadowRadius: 1,
 
 
+    }
+    ,
+    txt_nameD1: {
+        marginTop: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 50,
+        paddingRight: 50,
+        textAlign: 'center',
+        width: '100%',
+        borderRadius: 10,
+        alignSelf: 'flex-end',
+        fontSize: 14,
+        fontWeight: '900',
+        color: 'white',
+        backgroundColor: '#38C674',
+    },
+    txt_nameD2: {
+        marginTop: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 50,
+        paddingRight: 50,
+        textAlign: 'center',
+        width: '100%',
+        borderRadius: 10,
+        alignSelf: 'flex-end',
+        fontSize: 14,
+        fontWeight: '900',
+        color: 'white',
+        backgroundColor: '#0053CE',
+    },
+    txt_nameD3: {
+        marginTop: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 50,
+        paddingRight: 50,
+        textAlign: 'center',
+        width: '100%',
+        borderRadius: 10,
+        alignSelf: 'flex-end',
+        fontSize: 14,
+        fontWeight: '900',
+        color: 'white',
+        backgroundColor: '#EECE3B',
+
+    },
+    txt_nameD4: {
+        marginTop: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 50,
+        paddingRight: 50,
+        textAlign: 'center',
+        width: '100%',
+        borderRadius: 10,
+        alignSelf: 'flex-end',
+        fontSize: 14,
+        fontWeight: '900',
+        color: 'white',
+        backgroundColor: '#FF4155',
     },
     txt_name: {
         width: '100%',
         borderTopLeftRadius: 10,
         borderBottomLeftRadius: 10,
         alignSelf: 'flex-end',
-        fontSize: 20,
+        fontSize: 14,
         textAlign: 'left',
         fontWeight: '700',
-        color: '#49D88B',
+        color: 'white',
         textShadowColor: "#38C674",
         textShadowOffset: {
             width: 1,
             height: 1,
         },
         textShadowRadius: 1,
+    }
+    ,
+    txt_name2: {
+        width: '100%',
+        borderTopLeftRadius: 10,
+        borderBottomLeftRadius: 10,
+        alignSelf: 'flex-end',
+        fontSize: 18,
+        textAlign: 'left',
+        fontWeight: '700',
+        color: '#0053CE',
+
+        textShadowColor: "#38C674",
+        textShadowOffset: {
+            width: 1,
+            height: 1,
+        },
+        textShadowRadius: 1,
+    },
+    txt_kku: {
+        marginTop: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 50,
+        paddingRight: 50,
+        textAlign: 'center',
+        width: '100%',
+        borderRadius: 10,
+        alignSelf: 'flex-end',
+        fontSize: 14,
+        fontWeight: '900',
+        color: 'grey',
+        
     }
 })
